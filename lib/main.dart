9c8a0321d0b9fe5d3107df79aea292a5490636c6
OM TATHED <tathedom17@gmail.com>
2023-03-18 13:30:22 +0530

    App without Icon using Assets as Image

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,20 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text("Hello World!"),
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[500],
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text("I am Rich"),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: const Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
       ),
     ),
   );
